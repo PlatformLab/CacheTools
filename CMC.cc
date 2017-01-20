f2d3b2b495d8f5c2d3eef4482ffcc2f4535d92f5
Henry Qin <root@hq6.me>
2017-01-19 20:54:27 -0500

    Updated cache miss cost analyzer
    1. Remove histogram.
    2. Generate data to plot a cdf.

--- CMC.cc
+++ CMC.cc
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,12 +8,21 @@
 #include "PerfUtils/Cycles.h"
 #include "Stats.h"
 
-#define NUM_RUNS 1000000
+#define NUM_RUNS 3000000
 
 // Uncomment the following line to meausre only overheads
 //#define NULL_MEASURE
 
 using PerfUtils::Cycles;
+
+void ensureDirectory(const char* dir) {
+    struct stat st = {0};
+
+    if (stat(dir, &st) == -1) {
+        mkdir(dir, 0700);
+    }
+}
+
 /**
  * Cache Miss Cost: This tool attempts to measure the cost of various levels of
  * cache misses by gradually increasing the size of an in-memory iterated over
@@ -22,6 +33,13 @@ int main(int argc, char** argv){
         printf("Usage: ./CMC <array_size_in_bytes> [datadir]\n");
         return 1;
     }
+
+    const char* datadir = NULL;
+    if (argc > 2) {
+        datadir = argv[2];
+        ensureDirectory(datadir);
+    }
+
     size_t count =  atoi(argv[1])/ sizeof(uint8_t*);
     if (count < 1) {
         printf("Array size must be at least %lu bytes.\n", sizeof(uint8_t*));
@@ -44,7 +62,7 @@ int main(int argc, char** argv){
 
 
     // Begin measurements
-    uint64_t deltas[NUM_RUNS];
+    uint64_t *deltas = new uint64_t[NUM_RUNS];
     uint64_t startTime;
     uint64_t delta;
 
@@ -58,13 +76,12 @@ int main(int argc, char** argv){
         deltas[i] = delta;
     }
     free(memory);
-    printStatistics("Cache Statistics", deltas, NUM_RUNS, NULL);
-    printHistogram(deltas, NUM_RUNS, 0, 300, 10);
-
+    printStatistics("Cache Statistics", deltas, NUM_RUNS, datadir);
 
     // Force usage of output value so that compiler doesn't optimize away our
     // loop.
     FILE* fp = fopen("/dev/null", "w");
     fprintf(fp, "%p\n", cur);
     fclose(fp);
+    delete [] deltas;
 }
